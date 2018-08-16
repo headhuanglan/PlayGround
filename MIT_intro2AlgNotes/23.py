1463cd95cb3bda9c527835405d5d454559f5bcbe
L <headhuanglan122@gmail.com>
2018-08-16 16:31:05 -0500

    day3Intro2Alg

---
+++ MIT_intro2AlgNotes/23.py
@@ -0,0 +1,27 @@
+"""
+
+P={problem solvable in polynomial}
+
+Exp={problem solvable in exponential time}
+
+R={problem solvable in finite time}
+
+
+NP={decision problems solvable in polytime
+    via a "lucky" alg.
+--nonedeterministic model: alg makes guesses
+  decison problems with solutions that can be checked
+  in polynomial time
+  }
+
+P!=NP   big conjecture
+        can't engineer luck
+        generating solutions can be harder than checking them
+
+
+reductions: A->B
+     convert problem A to problem B
+
+   
+"""
+
